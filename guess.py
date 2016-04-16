db78ccc1f8262fa486d2d393c1b3b640d97bed8b
Paul <Paul>
2016-04-16 10:26:29 -0400

    Newer version

--- guess.py
+++ guess.py
@@ -6,19 +6,21 @@ Created on Mar 20, 2016
 from random import randint
 
 def main():
-    print "Welcome to the number guessing game!"
+    print "Welcome to the number guessing game! You will have 5 chances to guess."
     num = randint(1, 10)
+    guesses = 0
     print "I have my number..."
     while True:
         guess = raw_input("What is your guess [1 - 10]? ")
+        guesses += 1
         if int(guess) == num:
+            print "You got it! Thanks for playing!"
+            break
+        elif guesses == 5:
+            print "That's not correct and that's 5 guesses. The number was " + str(num) + "."
             break
         else:
-            if int(guess) < num:
-                print "That's too low, try again!"
-            else:
-                print "That's too high, try again!"
-    print "You got it! Thanks for playing!"            
+            print "Not correct, please try again."            
                 
 if __name__ == '__main__':
     main()
